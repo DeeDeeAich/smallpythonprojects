8a2135e82565f29b1bef52c75bd3f9225bf60978
DeeDeeAich <32936305+DeeDeeAich@users.noreply.github.com>
2020-09-26 08:54:13 -0500

    Update bookcalculator.py

--- bookcalculator.py
+++ bookcalculator.py
@@ -2,6 +2,6 @@ pages = input("How many pages do you have to read? ")
 time = input("In how many days do you have to read the book? ")
 
 total = int(pages) // int(time)
-print("You will have to read " + str(total) + " pages per day to finish in " + str(time) + " days." )
+print(f"You will have to read {total} pages per day in order to finish in {time} days.")
 
 input("Press enter to quit...")
